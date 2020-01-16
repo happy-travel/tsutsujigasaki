d0c94d5447790c540dc3ae16b3969963d0f18af4
Kirill Taran <kirill.taran@hotmail.com>
2020-01-17 02:28:40 +0300

    Database configuration added

---
+++ HappyTravel.CurrencyConverter/Data/CurrencyConverterContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HappyTravel.CurrencyConverter.Data
+{
+    public class CurrencyConverterContext : DbContext
+    {
+        public CurrencyConverterContext(DbContextOptions<CurrencyConverterContext> options) : base(options) {}
+    }
+}
