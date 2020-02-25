49e2ddbfe6b7ce269a3b2da6f29bae74ab154b50
Kirill Taran <kirill.taran@hotmail.com>
2020-02-26 01:34:25 +0300

    An index added on currencies to CurrencyRates table

--- HappyTravel.CurrencyConverter/Migrations/20200225222646_HashIndexOnCurrencyRates.Designer.cs
+++ HappyTravel.CurrencyConverter/Migrations/20200225222646_HashIndexOnCurrencyRates.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using HappyTravel.CurrencyConverter.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace HappyTravel.CurrencyConverter.Migrations
 {
     [DbContext(typeof(CurrencyConverterContext))]
-    partial class CurrencyConverterContextModelSnapshot : ModelSnapshot
+    [Migration("20200225222646_HashIndexOnCurrencyRates")]
+    partial class HashIndexOnCurrencyRates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
