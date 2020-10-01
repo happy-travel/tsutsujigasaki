ddb42e6e6d0279f01e93c3c51e65d02ccd49143b
Kirill Taran <kirill.taran@hotmail.com>
2020-10-01 19:23:47 +0300

    Conversion package added

---
+++ HappyTravel.CurrencyConverter/Converter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HappyTravel.Money.Enums;
+using HappyTravel.Money.Models;
+
+namespace HappyTravel.CurrencyConverter
+{
+    public class Converter
+    {
+        public Converter(in decimal rate, Currencies sourceCurrency, Currencies targetCurrency)
+        {
+            _rate = rate;
+            _sourceCurrency = sourceCurrency;
+            _targetCurrency = targetCurrency;
+
+            CheckPreconditions(in _rate, _sourceCurrency, _targetCurrency);
+        }
+
+
+        public MoneyAmount Convert(in MoneyAmount sourceValue)
+        {
+            if (sourceValue.Currency != _sourceCurrency)
+                throw new ArgumentException("The source amount currency n=mismatches with a predefined one.");
+
+            return Convert(in _rate, _targetCurrency, in sourceValue);
+        }
+
+
+        public MoneyAmount Convert(in decimal sourceValue) 
+            => Convert(in _rate, _targetCurrency, new MoneyAmount(sourceValue, _sourceCurrency));
+
+
+        public static MoneyAmount Convert(in decimal rate, Currencies targetCurrency, in MoneyAmount sourceValue)
+        {
+            var results = Convert(in rate, targetCurrency, new[] {sourceValue});
+            results.TryGetValue(sourceValue, out var result);
+
+            return result;
+        }
+        
+
+        public static MoneyAmount Convert(in decimal rate, Currencies sourceCurrency, Currencies targetCurrency, in decimal value) 
+            => Convert(in rate, targetCurrency, new MoneyAmount(value, sourceCurrency));
+
+
+        public Dictionary<MoneyAmount, MoneyAmount> Convert(IEnumerable<MoneyAmount> sourceValues)
+        {
+            if (sourceValues is null)
+                throw new ArgumentNullException(nameof(sourceValues));
+
+            var list = sourceValues.ToList();
+            if (list.FirstOrDefault().Currency != _sourceCurrency)
+                throw new ArgumentException("The source amount currency n=mismatches with a predefined one.");
+
+            return ConvertInternal(_rate, _targetCurrency, list);
+        }
+        
+        
+        public static Dictionary<MoneyAmount, MoneyAmount> Convert(in decimal rate, Currencies targetCurrency, IEnumerable<MoneyAmount> sourceValues)
+        {
+            if (sourceValues is null)
+                throw new ArgumentNullException(nameof(sourceValues));
+            
+            var list = sourceValues.ToList();
+            var sourceCurrency = list.FirstOrDefault().Currency;
+            CheckPreconditions(in rate, sourceCurrency, targetCurrency);
+
+            return ConvertInternal(in rate, targetCurrency, list);
+        }
+
+
+        private static void CheckPreconditions(in decimal rate, Currencies sourceCurrency, Currencies targetCurrency)
+        {
+            if (targetCurrency == Currencies.NotSpecified)
+                throw new ArgumentException("The target currency must be specified.");
+
+            if (sourceCurrency == Currencies.NotSpecified)
+                throw new ArgumentException("The source currency must be specified.");
+
+            if (sourceCurrency == targetCurrency && rate != 1m)
+                throw new ArgumentException("You use the same currency as a source and a target.");
+        }
+
+
+        private static Dictionary<MoneyAmount, MoneyAmount> ConvertInternal(in decimal rate, Currencies targetCurrency, List<MoneyAmount> sourceValues)
+        {
+            var results = new Dictionary<MoneyAmount, MoneyAmount>(sourceValues.Count);
+            foreach (var sourceValue in sourceValues)
+            {
+                if (results.ContainsKey(sourceValue))
+                    continue;
+
+                var targetAmount = new MoneyAmount(sourceValue.Amount * rate, targetCurrency);
+                results.Add(sourceValue, targetAmount);
+            }
+
+            return results;
+        }
+
+
+        private readonly decimal _rate;
+        private readonly Currencies _sourceCurrency;
+        private readonly Currencies _targetCurrency;
+    }
+}
---
+++ HappyTravel.CurrencyConverterTests/ConverterTests.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using HappyTravel.CurrencyConverter;
+using HappyTravel.Money.Enums;
+using HappyTravel.Money.Models;
+using Xunit;
+
+namespace HappyTravel.CurrencyConverterTests
+{
+    public class ConverterTests
+    {
+        [Theory]
+        [InlineData(Currencies.AED, Currencies.AED)]
+        [InlineData(Currencies.USD, Currencies.USD)]
+        public void Convert_should_throws_exception_when_source_and_target_currencies_are_same(Currencies sourceCurrency, Currencies targetCurrency)
+        {
+            Assert.Throws<ArgumentException>(() => Converter.Convert(1.5m, targetCurrency, new MoneyAmount(1m, sourceCurrency)));
+        }
+
+
+        [Theory]
+        [InlineData(Currencies.AED, Currencies.EUR)]
+        [InlineData(Currencies.AED, Currencies.USD)]
+        [InlineData(Currencies.AED, Currencies.AED)]
+        [InlineData(Currencies.USD, Currencies.AED)]
+        public void Convert_should_convert_from_source_currency_to_target_currency(Currencies sourceCurrency, Currencies targetCurrency)
+        {
+            var result = Converter.Convert(1m, sourceCurrency, targetCurrency, 0m);
+
+            Assert.Equal(targetCurrency, result.Currency);
+        }
+
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(10, 10)]
+        [InlineData(-10, -10)]
+        public void Convert_Should_convert_values(decimal rate, decimal expected)
+        {
+            var result = Converter.Convert(rate, Currencies.EUR, new MoneyAmount(1m, Currencies.USD));
+
+            Assert.Equal(expected, result.Amount);
+        }
+
+
+        [Fact]
+        public void Convert_should_skip_same_values()
+        {
+            var sources = new List<MoneyAmount>
+            {
+                new MoneyAmount(4m, Currencies.EUR),
+                new MoneyAmount(4m, Currencies.EUR),
+                new MoneyAmount(2m, Currencies.EUR)
+            };
+
+            var results = Converter.Convert(1m, Currencies.EUR, sources);
+
+            Assert.Equal(2, results.Count);
+        }
+
+
+        [Fact]
+        public void Convert_should_throw_exception_when_different_source_currency_had_passed_into_instance()
+        {
+            var amount = new MoneyAmount(1m, Currencies.AED);
+            var converter = new Converter(1m, Currencies.EUR, Currencies.USD);
+
+            Assert.Throws<ArgumentException>(() => converter.Convert(amount));
+        }
+
+
+        [Fact]
+        public void Convert_should_throw_exception_when_null_value_provided()
+        {
+            Assert.Throws<ArgumentNullException>(() => Converter.Convert(1m, Currencies.NotSpecified, null!));
+        }
+
+
+        [Fact]
+        public void Convert_should_throw_exception_when_source_currency_unspecified()
+        {
+            Assert.Throws<ArgumentException>(() => Converter.Convert(1m, Currencies.EUR, new MoneyAmount(1m, Currencies.NotSpecified)));
+        }
+
+
+        [Fact]
+        public void Convert_should_throw_exception_when_target_currency_unspecified()
+        {
+            Assert.Throws<ArgumentException>(() => Converter.Convert(1m, Currencies.NotSpecified, new MoneyAmount(1m, Currencies.EUR)));
+        }
+    }
+}
